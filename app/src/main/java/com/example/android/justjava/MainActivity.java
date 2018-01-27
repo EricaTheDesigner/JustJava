56cd5311060362dddd8dea8343c230b7faebf6e5
EricaTheDesigner <Erica@EricaTheDesigner.com>
2018-01-26 23:17:27 -0500

    updated MainActivity.java file and created new methods

--- app/src/main/java/com/example/android/justjava/MainActivity.java
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -22,6 +22,8 @@ import java.text.NumberFormat;
  */
 public class MainActivity extends AppCompatActivity {
 
+    int quantity = 2;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,10 +34,7 @@ public class MainActivity extends AppCompatActivity {
      * This method is called when the order button is clicked.
      */
     public void submitOrder(View view) {
-        int numberOfCoffees = 2;
-
-        display(numberOfCoffees);
-        displayPrice(numberOfCoffees * 5);
+        displayPrice(quantity * 5);
     }
 
     /**
@@ -53,5 +52,22 @@ public class MainActivity extends AppCompatActivity {
         TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
         priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
     }
+
+    /**
+     * This method increases the quantity
+     */
+    public void increment(View view) {
+        quantity = quantity * 2;
+        display(quantity);
+    }
+
+    /**
+     * This method decreases the quantity
+     */
+    public void decrement(View view) {
+        quantity = quantity / 2;
+        display(quantity);
+    }
+
 
 }
